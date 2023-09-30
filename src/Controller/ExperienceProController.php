388b57e28df457bd4b7fd8e18e0cfa96d5520513
LePariisien <alexandrecano@hotmail.fr>
2023-09-30 02:31:08 +0200

    add experience in ExperienceProPage & Controller

--- src/Controller/ExperienceProController.php
+++ src/Controller/ExperienceProController.php
@@ -5,14 +5,25 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\ExperiencePro;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ExperienceProController extends AbstractController
 {
     #[Route('/experience/pro', name: 'app_experience_pro')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        // Récupérez les expériences professionnelles pour l'utilisateur ayant un user_id de 1
+        $experiences = $entityManager->getRepository(ExperiencePro::class)->createQueryBuilder('exp')
+        ->where('exp.user = :user_id')
+        ->setParameter('user_id', 1)
+        ->getQuery()
+        ->getResult();
+
         return $this->render('experience_pro/index.html.twig', [
-            'controller_name' => 'ExperienceProController',
+            'experiences' => $experiences,
         ]);
     }
 }
+
+
