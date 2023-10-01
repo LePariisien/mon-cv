0b79490931762200f5fe77148cec2054b8a0313d
LePariisien <alexandrecano@hotmail.fr>
2023-10-01 03:41:15 +0200

    update ExperienceEtudiantPage

--- src/Controller/ExperienceEtuController.php
+++ src/Controller/ExperienceEtuController.php
@@ -5,14 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\ExperienceEtudiant; // Importe la classe ExperiencePro
+use Doctrine\ORM\EntityManagerInterface;
 
 class ExperienceEtuController extends AbstractController
 {
     #[Route('/experience/etu', name: 'app_experience_etu')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        // Réutilise le code pour récupérer les expériences professionnelles (à personnaliser si nécessaire)
+        $experiences_etu = $entityManager->getRepository(ExperienceEtudiant::class)->createQueryBuilder('exp')
+        ->where('exp.user = :user_id')
+        ->setParameter('user_id', 1)
+        ->getQuery()
+        ->getResult();
+
         return $this->render('experience_etu/index.html.twig', [
-            'controller_name' => 'ExperienceEtuController',
+            'experiences' => $experiences_etu,
         ]);
     }
 }
