b1763d56f2cfd7f07602d980028665a7c4f54cf5
LePariisien <alexandrecano@hotmail.fr>
2024-01-08 13:43:05 +0100

    update LoisirsPage

--- src/Controller/LoisirsController.php
+++ src/Controller/LoisirsController.php
@@ -11,19 +11,8 @@ class LoisirsController extends AbstractController
     #[Route('/loisirs', name: 'app_loisirs')]
     public function index(): Response
     {
-        $languages = [
-            'Anglais' => 3.5,
-            'Espagnol' => 3.5,
-            'Portugais' => 2,
-        ];
-
-        $travelDestinations = ['États-Unis', 'Thaïlande', 'Canada', 'Europe'];
-        $sports = ['Football', 'Tennis', 'Musculation'];
 
         return $this->render('loisirs/index.html.twig', [
-            'languages' => $languages,
-            'travelDestinations' => $travelDestinations,
-            'sports' => $sports,
         ]);
     }
 }
